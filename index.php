ff157694d2ca5755e4bfbb5e61c0249fa606938b
Marcos Freire <marcfreire@gmail.com>
2017-06-06 11:42:10 -0300

    first commit

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
-<?php
-echo "Hello Word!";
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <![endif]-->
+    <title></title>
+</head>
+<body>
+    
+
+</body>
+</html>
+
